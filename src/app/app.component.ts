7634a20ebc686dca497372dc4d53a2e36907a653
Max Hanna <max.hanna1+cae@cae.com>
2026-03-05 16:38:47 -0500

    facelift

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'AeroLimos';
   email = 'rafikhanna7@aol.com';
   phone = '5145948626';
+
+  ngOnInit(): void {
+    console.log('AppComponent initialized');
+  }
 }
