86cd00c83435e41fcbbe77ecaccdc67635558641
Max Hanna <max.hanna1+cae@cae.com>
2026-03-05 21:55:30 -0500

    phone numbers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,8 +9,18 @@ export class AppComponent implements OnInit {
   title = 'AeroLimos';
   email = 'aerolimosmontreal@gmail.com';
   phone = '5145948626';
+  showCallMenu = false;
 
   ngOnInit(): void {
     console.log('AppComponent initialized');
+  }
+
+  toggleCallMenu(event: Event): void {
+    event.stopPropagation();
+    this.showCallMenu = !this.showCallMenu;
+  }
+
+  closeCallMenu(): void {
+    this.showCallMenu = false;
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { ClickOutsideDirective } from './click-outside.directive';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ClickOutsideDirective],
   imports: [BrowserModule],
   bootstrap: [AppComponent]
 })
